0c8a632e014026f629984bb6ddb7f2c0b510979e
Blackflame796 <pws15548@gmail.com>
2025-09-05 23:51:58 +0300

    Add Work_1

---
+++ Work_1/Lab2/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void age() {
+    printf("   __    _   __    _   __   \n");
+    printf(" |   |  | |    |  | |    |  \n");
+    printf(" |   |  |_|    |  |_|    |  \n");
+}
+
+int main() {
+    age();
+    return 0;
+}
